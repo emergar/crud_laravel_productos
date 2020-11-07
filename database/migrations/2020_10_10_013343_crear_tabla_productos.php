702d3b7ee633789d7e4b5c9c5fc79f0744edbaa3
Emerson Garay <emerson.garay@gmail.com>
2020-11-06 22:54:38 -0500

    Respaldo 003 del CRUD Productos

---
+++ database/migrations/2020_10_10_013343_crear_tabla_productos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            //$table->double('peso');
+            //$table->double('volumen');
+            $table->double('precio');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
